20aeb25066659666a8a3654f4f1069110a32573c
AdaptCharm <jamie@jamie.no>
2021-09-01 23:12:30 +0200

    Initial upload

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import { Layout } from '@components/common'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <>
+      <div className="relative bg-white dark:bg-black pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="mx-auto max-w-5xl p-6 sm:px-6 lg:px-8">
+          <div>
+            <h1 className="mb-12 text-2xl md:text-2xl font-medium text-black dark:text-white">
+              Jamie Isaksen
+            </h1>
+            <h2 className="text-6xl md:text-8xl tracking-wide font-bold text-black dark:text-white">
+              Full stack developer based in Norway.
+            </h2>
+            <h2 className="mt-28 text-6xl md:text-8xl tracking-wide font-bold text-black dark:text-white">
+              CEO at the car rental company, Innov.
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
+
+Home.Layout = Layout
