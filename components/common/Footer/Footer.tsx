79026823f810652339156d59b3e949d054db6256
AdaptCharm <jamie@jamie.no>
2021-09-05 19:19:54 +0200

    Minor changes to the footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -13,7 +13,8 @@ const Footer: FC = () => {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center">
           <p className="text-black dark:text-white">
-            &copy; {new Date().getFullYear()} Jamie Isaksen
+            &copy; {new Date().getFullYear()} Jamie Isaksen. Get in touch at
+            jamie@jamie.no.
           </p>
         </div>
       </div>
