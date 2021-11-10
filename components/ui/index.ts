46a0524d206303c95122ef7f480e2ee8f5d6ebbe
AdaptCharm <jamie@jamie.no>
2021-11-10 18:56:15 +0100

    Remove unused UI component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from './Button'
-export { default as Badge } from './Badge'
 export { default as Tooltip } from './Tooltip'
 export { default as Popover } from './Popover'
 export { default as Transition } from './Transition'
