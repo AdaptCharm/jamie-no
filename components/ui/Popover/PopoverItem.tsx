1aed76dea5a834d420575805dfe7d5f1c8da04b1
AdaptCharm <jamie@jamie.no>
2021-11-10 22:03:56 +0100

    Fix memo bug

--- components/ui/Popover/PopoverItem.tsx
+++ components/ui/Popover/PopoverItem.tsx
@@ -19,30 +19,34 @@ const defaultProps = {
 type NativeAttrs = Omit<HTMLAttributes<any>, keyof Props>
 export type PopoverItemProps = Props & typeof defaultProps & NativeAttrs
 
-const PopoverItem: FC<PopoverItemProps> = memo(
-  ({ line, title, item, className, children, ...props }) => {
-    return (
-      <>
-        <div
-          className={cn(
-            {
-              'transition-colors ease-[ease] duration-100 ': title || item,
-              'px-5 py-2 text-black dark:text-white': title,
-              'my-2 border-t-[1px] border-accents-2 dark:border-accents-8':
-                line,
-              'py-2 px-5 text-sm max-w-full hover:bg-accents-1 dark:hover:bg-accents-9 text-accents-5 dark:text-accents-5 hover:text-black dark:hover:text-white':
-                item,
-            },
-            className
-          )}
-          {...props}
-        >
-          {children}
-        </div>
-        {title && <PopoverItem line item={false} title={false} className="" />}
-      </>
-    )
-  }
-)
+const PopoverItem: FC<PopoverItemProps> = ({
+  line,
+  title,
+  item,
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <>
+      <div
+        className={cn(
+          {
+            'transition-colors ease-[ease] duration-100 ': title || item,
+            'px-5 py-2 text-black dark:text-white': title,
+            'my-2 border-t-[1px] border-accents-2 dark:border-accents-8': line,
+            'py-2 px-5 text-sm max-w-full hover:bg-accents-1 dark:hover:bg-accents-9 text-accents-5 dark:text-accents-5 hover:text-black dark:hover:text-white':
+              item,
+          },
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+      {title && <PopoverItem line item={false} title={false} className="" />}
+    </>
+  )
+}
 
-export default withDefaults(PopoverItem, defaultProps)
+export default withDefaults(memo(PopoverItem), defaultProps)
